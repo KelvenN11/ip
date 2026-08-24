85c303ae27866e929c2f62f20a4b3603ce44415c
KelvenN11 <kelvennathanael11@gmail.com>
2026-08-24 16:18:14 +0800

    Duke.java: Greet user and exit with BOT identity
    Implements the Level-0 requirement: print a banner, greet the user
    by name, and print a goodbye message on exit. Renamed the chatbot
    from Duke to BOT and updated the ASCII banner to match.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        String name = "BOT";
+        String banner = " ____   ___  _____ \n"
+                + "| __ ) / _ \\|_   _|\n"
+                + "|  _ \\| | | | | |  \n"
+                + "| |_) | |_| | | |  \n"
+                + "|____/ \\___/  |_|  \n";
+        String divider = "____________________________________________________________";
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Hello! I'm " + name + ".");
+        System.out.println("What can I do for you?");
+        System.out.println(divider);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
